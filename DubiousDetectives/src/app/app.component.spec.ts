a2508eef316f664e0d5885a6c4a46a8dbec5aa34
Toby <toby.br314@gmail.com>
2019-11-16 22:34:29 +0000

    Back to boilerplate

--- DubiousDetectives/src/app/app.component.spec.ts
+++ DubiousDetectives/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'DubiousDetectives'`, () => {
+  it(`should have as title 'dubiousdetectives'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('DubiousDetectives');
+    expect(app.title).toEqual('dubiousdetectives');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('DubiousDetectives app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('dubiousdetectives app is running!');
   });
 });
--- DubiousDetectives/src/app/app.module.ts
+++ DubiousDetectives/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './components/app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
